005b14b3d504ade53b8e17b4f5b792f9bf42a161
zacharywoo23 <99620526+zacharywoo23@users.noreply.github.com>
2022-03-09 19:34:53 -0800

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,23 @@
+# decorator to track number of times a function is called
+def func_counter(method):
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1  # every time a function is called, add 1 to the call counter
+        return method(*args, **kwargs)
+
+    wrapper.calls = 0
+    return wrapper
+
+# test the func_counter()
+@func_counter
+def spanishGreeting():
+    print("Hola amigos")
+
+x = 0
+while (x < 10):
+    spanishGreeting()
+    x += 1
+
+nCalls = spanishGreeting.calls
+print(nCalls)
+
+
